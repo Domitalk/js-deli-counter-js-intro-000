b2b84f48f8cba484514354c1c0ac50a25dfaa6ca
Dominic <dominic.m.chu@gmail.com>
2019-08-07 08:19:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(katzDeliLine) {
-  var lineLong
+  var lineLong = []
   if (katzDeliLine.length === 0) {
     return `The line is currently empty.`;
   }
